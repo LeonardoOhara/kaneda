2ff7edbdd6b194c3e752ccb26765e7b1e4ca2601
LeonardoOhara <leonardo.ohara123@gmail.com>
2024-05-21 13:51:59 -0300

    Update
    Criei um efeito reveal com a biblioteca scrollreveal.

---
+++ script.js
@@ -0,0 +1,17 @@
+window.revelar = ScrollReveal({reset:true})
+
+// SOBRE NOS //
+
+revelar.reveal('.texto1',{
+    origin: 'left',
+    distance: '400px',
+    duration: 2000,
+    reset: true
+})
+
+revelar.reveal('.imgsobre',{
+    origin: 'right',
+    distance: '400px',
+    duration: 2000,
+    reset: true
+})
