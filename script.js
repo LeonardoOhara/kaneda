a8503cbe58f7672e4a81d7309ae549dc00fe68a4
LeonardoOhara <leonardo.ohara123@gmail.com>
2024-06-18 13:45:59 -0300

    update
    adicionado o menu para mobile e melhorado o alinhamento dos elementos

--- script.js
+++ script.js
@@ -28,6 +28,31 @@ revelar.reveal('.slider',{
     duration: 2000,
 })
 
+// MEMU BAR RESPONSIVE //
+
+document.addEventListener('DOMContentLoaded', () => {
+    const btnAbrirMenu = document.querySelector('.btn-abrir-menu');
+    const btnFecharMenu = document.querySelector('.btn-fechar');
+    const menuMobile = document.querySelector('.menu-mobile');
+    const overlayMenu = document.querySelector('.overlay-menu');
+
+    // Função para abrir o menu
+    btnAbrirMenu.addEventListener('click', () => {
+        menuMobile.classList.add('abrir-menu');
+    });
+
+    // Função para fechar o menu
+    btnFecharMenu.addEventListener('click', () => {
+        menuMobile.classList.remove('abrir-menu');
+    });
+
+    // Fechar o menu ao clicar fora dele
+    overlayMenu.addEventListener('click', () => {
+        menuMobile.classList.remove('abrir-menu');
+    });
+});
+
+
 
 
 
